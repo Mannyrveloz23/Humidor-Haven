e66853e207d9bbb4a330691a93e30ddcf25816f9
mannyrveloz23 <you@example.com>
2023-05-01 23:25:46 -0400

    d

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,32 +1,5 @@
 const mongoose = require('mongoose');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Mannyrveloz23@live.com:Samara10.@humidor-haven.bwncniw.mongodb.net/humidor-haven?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/humidor-haven');
 
 module.exports = mongoose.connection;
